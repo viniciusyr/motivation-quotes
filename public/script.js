8b4b84ed92e09e1f1d44e63df25a01573ec1b8ba
Vinicius Rodrigues <vinicius.worms@hotmail.com>
2024-08-20 21:15:09 +0800

    Finishing design, onclick function and Google search query of author name

--- public/script.js
+++ public/script.js
@@ -1,19 +1,38 @@
 import { getURL } from './quotesAPI.js';
 
+let quoteText = '';
+let authorText = '';
+
 document.addEventListener('DOMContentLoaded', async () => {
+    await fetchAndDisplayQuote();
+});
+
+async function fetchAndDisplayQuote() {
     try {
-        const { authorText, quoteText } = await getURL();
-        if (authorText && quoteText) {
+        const { authorText: newAuthorText, quoteText: newQuoteText } = await getURL();
+        if (newAuthorText && newQuoteText) {
+            quoteText = newQuoteText;
+            authorText = newAuthorText;
+            
             const quoteContainer = document.getElementById('quote-container');
             const authorContainer = document.getElementById('author-container');
             if (quoteContainer && authorContainer) {
                 quoteContainer.innerText = quoteText;
-                authorContainer.innerText = authorText;
+                
+                const searchQuery = authorText.replace(/\s+/g, '+'); 
+                const searchLink = `https://www.google.com/search?q=${searchQuery}`;
+                authorContainer.innerHTML = `<a href="${searchLink}" target="_blank">${authorText}</a>`;
             } else {
-                console.error('Elementos HTML não encontrados.');
+                console.error('Error: The containers were not found!');
             }
         }
     } catch (error) {
-        console.error('Error in script:', error);
+        console.error('Error:', error);
     }
-});
+}
+
+function refreshQuote() {
+    fetchAndDisplayQuote(); 
+}
+
+window.refreshQuote = refreshQuote;
